8bcac06ed33a556ae093b4dac74b20fff048e92f
AndreyKosov <andrey.kosov.05@inbox.ru>
2025-02-26 18:56:46 +0500

    26.02.2025

---
+++ scripts/neo4j_graph.py
@@ -0,0 +1,74 @@
+import re
+from pathlib import Path
+from neo4j import GraphDatabase
+
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+FILE_PATH = BASE_DIR / "notebooks" / "ТИУ Сущности База знаний.txt"
+
+uri = "bolt://localhost:7687"
+
+driver = GraphDatabase.driver(uri)
+
+
+with open(FILE_PATH, mode="r", encoding="utf-8") as file:
+    data = file.read()
+
+print(f"---Size of file: {len(data)}---")
+
+
+entity_pattern = re.compile(r'\(entity\|([^|]+)\|([^|]+)\|([^|]+)\)')
+relationship_pattern = re.compile(r'\(relationship\|([^|]+)\|([^|]+)\|([^|]+)\|([^|]+)\)')
+
+
+entities: list[dict] = []
+for match in entity_pattern.finditer(data):
+    entity_name, entity_type, entity_description = match.groups()
+    entities.append({
+        "name": entity_name.strip(),
+        "type": entity_type.strip(),
+        "description": entity_description.strip()
+    })
+print(f"---Count of entities: {len(entities)}---")
+print(entities[1575])
+
+relationships: list[dict] = []
+for match in relationship_pattern.finditer(data):
+    source, target, relation_type, relation_description = match.groups()
+    relationships.append({
+        "source": source.strip(),
+        "target": target.strip(),
+        "type": relation_type.strip(),
+        "description": relation_description.strip()
+    })
+print(f"---Count of relationships: {len(relationships)}---")
+
+
+def add_entity(tx, entity) -> None:
+    tx.run(
+        "CREATE (e:Entity {name: $name, type: $type, description: $description})",
+        name=entity["name"], type=entity["type"], description=entity["description"]
+    )
+
+
+def add_relationship(tx, relation) -> None:
+    tx.run("""
+        MATCH (a:Entity {name: $source}), (b:Entity {name: $target})
+        CREATE (a)-[r:RELATION {type: $type, description: $description}]->(b)
+        """,
+        source=relation["source"],
+        target=relation["target"],
+        type=relation["type"],
+        description=relation["description"]
+    )
+
+
+'''with driver.session() as session:
+    for entity in entities:
+        session.execute_write(add_entity, entity)
+
+    for relation in relationships:
+        session.execute_write(add_relationship, relation)'''
+
+driver.close()
---
+++ scripts/read_neo4j.py
@@ -0,0 +1,28 @@
+from neo4j import GraphDatabase
+
+
+uri = "bolt://localhost:7687"
+
+driver = GraphDatabase.driver(uri)
+
+def fetch_graph_by_entity(tx, entity_name):
+    query = """
+    MATCH (a:Entity {name: $entity_name})-[r]->(b)
+    RETURN a, r, b
+    """
+    result = tx.run(query, entity_name=entity_name)
+    return result.data()
+
+entity_name = "ЭКОНОМИЧЕСКОЕ_УПРАВЛЕНИЕ_ПРЕДПРИЯТИЯМИ_ТОПЛИВНО-ЭНЕРГЕТИЧЕСКОГО_КОМПЛЕКСА"
+
+with driver.session() as session:
+    graph_data = session.execute_read(fetch_graph_by_entity, entity_name)
+    print(graph_data)
+    for record in graph_data:
+        source = record['a']['name']
+        target = record['b']['name']
+        relation_type = record['r']
+        relation_description = record['r']
+        print(f"Сущность: {source} -> {target} | Тип связи: {relation_type} | Описание: {relation_description}")
+
+driver.close()
