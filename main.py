f2a4d42d40a6b0b139c5996989be4e5e8b524f05
AndreyKosov <andrey.kosov.05@inbox.ru>
2025-05-14 14:54:23 +0500

    14.05.2025 fix main entry point

--- main.py
+++ main.py
@@ -5,7 +5,9 @@ import uvicorn
 from src.tyuiu_gpt.api.app import create_fastapi_app
 
 
+app = create_fastapi_app()
+
+
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
-    app = create_fastapi_app()
     uvicorn.run(app=app, host="localhost", port=8000)
