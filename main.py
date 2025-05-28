0641d239b650a033b4b9830a0a6c817933cb5be5
AndreyKosov <andrey.kosov.05@inbox.ru>
2025-05-28 10:05:00 +0500

    28.05.2025 Refactor applicant entry point

--- main.py
+++ main.py
@@ -1,13 +1,8 @@
 import logging
 
-import uvicorn
-
 from src.tyuiu_gpt.api.app import create_fastapi_app
 
 
+logging.basicConfig(level=logging.INFO)
+
 app = create_fastapi_app()
-
-
-if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO)
-    uvicorn.run(app=app, host="localhost", port=8000)
